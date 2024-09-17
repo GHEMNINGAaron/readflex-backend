88429454dc28038f84eb76e823071bd5bb70e11a
GHEMNING Aaron <aaron.ghemning@institutsaintjean.com>
2024-09-17 23:10:46 +0200

    Push base application

---
+++ src/main/java/com/example/groupe2/readflex/ReadflexApplication.java
@@ -0,0 +1,13 @@
+package com.example.groupe2.readflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadflexApplication.class, args);
+	}
+
+}
